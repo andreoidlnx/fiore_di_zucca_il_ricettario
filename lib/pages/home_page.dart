edb1d5bce0059a7aec2f7b1df5f30d690206a2f2
Andrea Panebianco <andreoidlnx@gmail.com>
2024-04-20 11:03:51 +0200

    fixed behaviour of app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,33 +12,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _scrollController = ScrollController();
+  bool _isAppBarExpanded = true;
+
   @override
   Widget build(BuildContext context) {
+
+    _scrollController.addListener(() {
+      setState(() {
+        const double maxExtent = 300.0;
+        final double minExtent = kToolbarHeight + MediaQuery.of(context).padding.top;
+
+        _isAppBarExpanded = _scrollController.hasClients &&
+            _scrollController.offset < (maxExtent - minExtent);
+      });
+    });
+
     return Scaffold(
       body: CustomScrollView(
+        controller: _scrollController,
         slivers: [
           // APP BAR
           SliverAppBar(
             pinned: true,
             snap: true,
             floating: true,
-            //title: Text(Constants.appTitle),
-            /* titleTextStyle: const TextStyle(
-              letterSpacing: 1.0,
-              fontSize: 20.0,
-              color: Colors.black
-            ), */
             expandedHeight: 300.0,
             elevation: 0.0,
+            title: !_isAppBarExpanded ? 
+            Image.asset(
+              "lib/images/logo_no_bg.png",
+              width: 40.0,
+              height: 40.0,
+            ) : null,
+            backgroundColor: const Color.fromARGB(255, 254, 231, 118),
             flexibleSpace: FlexibleSpaceBar(
               background: Container(
-                color: const Color.fromARGB(255, 255, 228, 89),
+                color: const Color.fromARGB(255, 254, 231, 118),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
                     const SizedBox(height: 2.0,),
                     Image.asset(
-                      'lib/images/logo.png',
+                      'lib/images/logo_no_bg.png',
                       height: 120.0,
                       width: 120.0,
                     ),
@@ -55,12 +71,14 @@ class _HomePageState extends State<HomePage> {
                 onPressed: () {}, 
                 icon: const Icon(Iconsax.heart),
                 color: Colors.black,
+                iconSize: 27.0,
               ),
-              IconButton(
+              !_isAppBarExpanded ? IconButton(
                 onPressed: () {}, 
                 icon: const Icon(Iconsax.search_normal_1),
                 color: Colors.black,
-              ),
+                iconSize: 27.0,
+              ) : const SizedBox.shrink(),
               const SizedBox(width: 10.0,),
             ],
           ),
@@ -71,12 +89,13 @@ class _HomePageState extends State<HomePage> {
               (BuildContext context, int index) {
                 return const Padding(
                   padding: EdgeInsets.all(15.0),
-                  child: RecipeWidget(title: "FIori di zucca in pastella", description: "Fritti e buonissimi", imagePath: "lib/images/logo.png",)
+                  child: RecipeWidget(title: "Fiori di zucca in pastella", description: "Fritti e buonissimi", imagePath: "lib/images/logo.png",)
                 );
               },
               childCount: 10,
             ),
           ),
+
         ],
       ),
     );
